9768fcb021fa721216fafaf118a3e19b1faa1b88
pabloosal <pablo2332002@gmail.com>
2026-02-09 19:49:37 +0100

    Añadido modelo Usuario

---
+++ RentManager/Models/Usuariocs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RentManager.Models
+{
+    public class Usuario
+    {
+        public int IdUsuario { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+    }
+}
