32f1bdf4c480725add5e9a88c9195c990152010c
pabloosal <pablo2332002@gmail.com>
2026-02-11 18:05:02 +0100

    Creado modelo Inquilino

---
+++ RentManager/Models/Inquilino.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RentManager.Models
+{
+    internal class Inquilino
+    {
+        public int IdInquilino { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public string Apellidos { get; set; } = string.Empty;
+        public string Dni { get; set; } = string.Empty;
+        public string Telefono { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public DateTime FechaAlta { get; set; } = DateTime.Now;
+        public string Observaciones { get; set; } = string.Empty;
+    }
+}
